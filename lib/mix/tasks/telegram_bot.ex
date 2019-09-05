516b1c2c429ab82652e849ba1f188facfdf78829
RuslanKhabibullin <khabibullin.ruslan.95@gmail.com>
2019-09-05 18:59:04 +0300

    infinite mix task fix

--- lib/mix/tasks/telegram_bot.ex
+++ lib/mix/tasks/telegram_bot.ex
@@ -10,7 +10,19 @@ defmodule Mix.Tasks.TelegramBot do
 
   @shortdoc "Starts telegram bot worker"
   def run(_) do
-    Logger.log(:info, "Stating the telegram bot...")
-    Bot.Supervisor.start_link
+    # TODO: Refactor this. On Mix tasks deps not initialized.
+    HTTPoison.start()
+    task = Task.async(&recursive_supervisor/0)
+    # Mix task exits on sync code finished. Dirty fix for infinite mix task
+    Task.await(task, :infinity)
+  end
+
+  defp recursive_supervisor do
+    if Process.whereis(:bot_supervisor) == nil do
+      Logger.log(:info, "Stating the telegram bot...")
+      Bot.Supervisor.start_link
+    end
+    
+    if Mix.env() != :test, do: recursive_supervisor(), else: nil
   end
 end
